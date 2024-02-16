e976c0950aebf23796ba6945fcc99a0e58c0502d
Jeeshan Khan <jeeshankhan@Jeeshans-MacBook-Air.local>
2024-02-17 03:32:50 +0530

    change root folder

--- app.js
+++ app.js
@@ -1,32 +1,36 @@
 import express from "express";
-const app = express()
-import passport from 'passport';;
-import expressSession from "express-session"
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import passportstrt from './utils/passport.js'
+const app = express();
+import passport from "passport";
+import expressSession from "express-session";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import passportstrt from "./src/utils/passport.js";
 
-
-app.use(express.json({ limit: "16kb" }))
-app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
-app.use(expressSession({ secret: process.env.JWT_SECRET, resave: false, saveUninitialized: false }))
-app.use(passport.session())
+app.use(
+  expressSession({
+    secret: process.env.JWT_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.session());
 app.use(passport.initialize());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-passportstrt()
+passportstrt();
 
 // routes import
-import userRouter from './routes/user.route.js'
-import postRouter from './routes/post.route.js'
-import countActive from './routes/user.count.js'
-
+import userRouter from "./src/routes/user.route.js";
+import postRouter from "./src/routes/post.route.js";
+import countActive from "./src/routes/user.count.js";
 
 // routes declaration
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/post", postRouter)
-app.use("/api/v1/test", countActive)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/post", postRouter);
+app.use("/api/v1/test", countActive);
 
-export { app }
+export { app };
---
+++ src/routes/user.count.js
@@ -0,0 +1,69 @@
+import express from "express";
+import passport from "passport";
+import Post from "../models/post.model.js";
+const router = express.Router();
+import isAuthenticated from "../middleware/auth.js";
+
+// router.use(passport.authenticate('local', { session: false }));
+
+// Task 5: Retrieve posts using latitude and longitude
+router.get("/geo", isAuthenticated, async (req, res) => {
+  try {
+    const { latitude, longitude } = req.query;
+
+    // Log the indexes
+    Post.collection
+      .getIndexes({ full: true })
+      .then((indexes) => {
+        // console.log("indexes:", indexes);
+      })
+      .catch(console.error);
+
+    // Recreate the index
+    await Post.collection.dropIndex("geoLocation_2dsphere");
+    await Post.collection.createIndex({ geoLocation: "2dsphere" });
+
+    // Check the data
+    const Posts = await Post.findOne({});
+    // console.log('geoLocation:', samplePost.geoLocation);
+
+    // Query with $geoNear
+    const post = await Post.find({
+      geoLocation: {
+        $near: {
+          $geometry: {
+            type: "Point",
+            coordinates: [longitude, latitude],
+          },
+          $maxDistance: 10000,
+        },
+      },
+    });
+    res.json({ Posts, post });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error fetching posts" });
+  }
+});
+
+// Task 5: Show the count of active and inactive posts in the dashboard
+router.get("/post-count", isAuthenticated, async (req, res) => {
+  try {
+    const userId = req.user.id;
+    // console.log(userId, "sdlkas");
+    const activePostCount = await Post.countDocuments({
+      createdBy: userId,
+      active: true,
+    });
+    const inactivePostCount = await Post.countDocuments({
+      createdBy: userId,
+      active: false,
+    });
+
+    res.json({ activePostCount, inactivePostCount });
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching post counts" });
+  }
+});
+
+export default router;
